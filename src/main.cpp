aad363f5b5392135379bcf78cf926944b11575ba
Karol <Szlagowski.k.j@gmail.com>
2024-05-15 20:25:43 +0200

    Dziala

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,9 @@ void setSystem(int distance);
 #define sensor0_echo_pin 6
 #define sensor1_trig_pin 0
 #define sensor1_echo_pin 5
-#define sensor2_trig_pin 18
-#define sensor2_echo_pin 19
-#define sensor3_trig_pin 20
-#define sensor3_echo_pin 21
-long duration[4];
+#define sensor2_trig_pin 1
+#define sensor2_echo_pin 10
+long duration[3];
 long average_duration;
 int distance;
 
@@ -34,15 +32,12 @@ void setup()
   pinMode(sensor0_trig_pin, OUTPUT);
   pinMode(sensor1_trig_pin, OUTPUT);
   pinMode(sensor2_trig_pin, OUTPUT);
-  pinMode(sensor3_trig_pin, OUTPUT);
   pinMode(sensor0_echo_pin, INPUT);
   pinMode(sensor1_echo_pin, INPUT);
   pinMode(sensor2_echo_pin, INPUT);
-  pinMode(sensor3_echo_pin, INPUT);
   digitalWrite(sensor0_trig_pin, LOW);
   digitalWrite(sensor1_trig_pin, LOW);
   digitalWrite(sensor2_trig_pin, LOW);
-  digitalWrite(sensor3_trig_pin, LOW);
 
   FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050);
   FastLED.setBrightness(BRIGHTNESS);
@@ -75,12 +70,32 @@ void measureDistance()
   duration[0] = measureTime(sensor0_echo_pin, sensor0_trig_pin);
   delayMicroseconds(10);
   duration[1] = measureTime(sensor1_echo_pin, sensor1_trig_pin);
-  // duration[2] = measureTime(sensor2_echo_pin, sensor2_trig_pin);
-  // duration[3] = measureTime(sensor3_echo_pin, sensor3_trig_pin);
-  // for (int i = 0; i < 4; i++)
-  //{
-  average_duration = (duration[0] + duration[1]) / 2;
-  //}
+  delayMicroseconds(10);
+  duration[2] = measureTime(sensor2_echo_pin, sensor2_trig_pin);
+
+  if (duration[0] < duration[1])
+  {
+    if (duration[0] < duration[2])
+    {
+      average_duration = duration[0];
+    }
+    else
+    {
+      average_duration = duration[2];
+    }
+  }
+  else
+  {
+    if (duration[1] < duration[2])
+    {
+      average_duration = duration[1];
+    }
+    else
+    {
+      average_duration = duration[2];
+    }
+  }
+
   distance = average_duration * 0.034 / 2;
 }
 
@@ -88,7 +103,7 @@ void setSystem(int distance)
 {
   // red
   FastLED.clear();
-  if (distance > 30 && distance < 40)
+  if (distance > 15 && distance < 20)
   {
     for (int i = 0; i <= 1; i++)
     {
@@ -101,7 +116,7 @@ void setSystem(int distance)
     analogWrite(BUZZER_PIN, 0);
   }
   // orange
-  else if (distance > 20 && distance < 30)
+  else if (distance > 10 && distance < 15)
   {
     FastLED.clear();
     for (int i = 0; i <= 1; i++)
@@ -120,7 +135,7 @@ void setSystem(int distance)
     analogWrite(BUZZER_PIN, 0);
   }
   // yellow
-  else if (distance > 10 && distance < 20)
+  else if (distance > 5 && distance < 10)
   {
     FastLED.clear();
     for (int i = 0; i <= 1; i++)
@@ -142,7 +157,7 @@ void setSystem(int distance)
     analogWrite(BUZZER_PIN, 0);
   }
   // green
-  else if (distance > 0 && distance < 10)
+  else if (distance > 0 && distance < 5)
   {
     FastLED.clear();
     for (int i = 0; i <= 1; i++)
